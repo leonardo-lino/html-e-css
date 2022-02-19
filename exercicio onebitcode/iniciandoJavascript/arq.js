11edded3613856ba291d1f477df7ae0e102b6309
Leonardo Lino <leonardo.lino@outlook.com.br>
2022-02-19 20:57:12 -0300

    exercicios feitos onebitcode

---
+++ exercicio onebitcode/iniciandoJavascript/arq.js
@@ -0,0 +1,8 @@
+nameO = prompt("Qual o nome da pessoa mais velha?")
+ageO = prompt("qual a idade da pessoa mais velha?")
+nameY = prompt("Qual o nome da pessoa mais nova?")
+ageY = prompt("Qual a idade da pessoa mais nova?")
+var dif = ageO - ageY
+alert("A pessoa mais velha é " + nameO + " e a idade dela é " + ageO)
+alert("A pessoa mais nova é " + nameY + " e a idade dela é " + ageY + " anos")
+alert('a diferença de idade delas duas é ' + dif + " anos")
